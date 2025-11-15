e769b16eac0b206fa30f910431e37fb56fedefc3
mathisweil <mathis.weil@outlook.com>
2025-11-15 14:32:54 +0000

    OSSSSSS

--- src/agent_core/agent.py
+++ src/agent_core/agent.py
@@ -1,5 +1,7 @@
+import os
+from dotenv import load_dotenv
+
 load_dotenv()
 
-openai_key = os.getenv("OPENAI_API_KEY")
 holistic_key = os.getenv("HOLISTIC_AI_API_TOKEN")
 valyu_key = os.getenv("VALYU_API_KEY")
